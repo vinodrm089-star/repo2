0fb468ad49f2b5a921f57ddd11da9454d5457c61
vinodrm089-star <vinodrm089@gmail.com>
2025-12-12 12:38:25 +0530

    C5

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
 from flask import Flask
 import os
-
-
-
+ 
 app = Flask(__name__)
-
-
-
+ 
 @app.route("/")
-
-
-
+ 
 def hello():
-    return "<h1>Sample Python WebApp CI/CD demo!!!!!</h1>"
-
-
-
+    return "<h1>Sample Python WebApp CI/CD demo!!!!!</h1>"
+ 
 if __name__ == "__main__":
-    
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
