f099c10f38a708db0648f70dc30c8b24c77b2206
vinodrm089-star <vinodrm089@gmail.com>
2025-12-12 12:40:56 +0530

    C6

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
  
 def hello():
-    return "<h1>Sample Python WebApp CI/CD demo!!!!!</h1>"
+    return "<h1>Sample Python WebApp CI/CD demo --- New Version!!!!!</h1>"
  
 if __name__ == "__main__":
     
